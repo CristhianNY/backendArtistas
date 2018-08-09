a45180b44137fa6e63075c92b60d71cea5cb21e8
cristhian Bonilla <cristhian4545@gmail.com>
2018-08-08 20:24:00 -0500

    First commit

---
+++ src/main/java/com/artistasamerica/ArtistasAmericaApplication.java
@@ -0,0 +1,12 @@
+package com.artistasamerica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtistasAmericaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtistasAmericaApplication.class, args);
+	}
+}
